43cf0ff888bdc483d77dcea807fe46468d48f4b4
kamioka24 <keee2404@gmail.com>
2020-09-16 18:10:11 +0900

    [Finish]

---
+++ ch_2.sql
@@ -0,0 +1,10 @@
+-- areaIDはusersテーブルとareaテーブル両方に存在するため、area.で指定する必要がある。
+SELECT userID, name, area.areaID, area_name -- 複数ある場合はカンマ(,)で区切る。
+FROM users
+  INNER JOIN jobs ON jobs.jobID = users.jobID
+  -- jobsテーブルをjobIDでusersテーブルと連結させている。
+  INNER JOIN area ON area.areaID = users.areaID
+  -- areaテーブルをareaIDでusersテーブルと連結させている。
+WHERE
+  userID >= 10 && userID < 20;
+-- 条件が複数の場合は && や AND で繋ぐ。
